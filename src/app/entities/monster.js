675993a11debf01ba9d88b47e68171db2e430b74
Sascha Depold <sdepold@ebay.com>
2019-08-21 20:58:21 +0200

    Add shadow and highlight of selected monster

--- src/app/entities/monster.js
+++ src/app/entities/monster.js
@@ -1,4 +1,5 @@
 import kontra from "kontra";
+import { addShadow } from "../misc/helper";
 
 export default class Monster {
   getSprites() {
@@ -21,7 +22,7 @@ export default class Monster {
       });
 
       this.sprite = kontra.Sprite({
-        type: 'monster',
+        type: "monster",
         x: 100,
         y: 100,
         dx: Math.random() * 4 - 2,
@@ -32,18 +33,26 @@ export default class Monster {
         animations: spriteSheet.animations
       });
 
+      addShadow(this.sprite, { x: -5 });
+
       const originalUpdate = this.sprite.update.bind(this.sprite);
 
-      this.sprite.update = function () {
+      this.sprite.update = function() {
         const canvas = kontra.getCanvas();
 
         originalUpdate();
 
-        if (this.x < 10 || this.x > (canvas.width - 32)) {
+        if (this.targeted) {
+          this.shadowColor = "rgba(250, 100, 100, 0.7)"
+        } else {
+          this.shadowColor = "rgba(0, 0, 0, 0.5)";
+        }
+
+        if (this.x < 10 || this.x > canvas.width - 32) {
           this.dx *= -1;
         }
 
-        if (this.y < 16 || this.y > (canvas.height - 82)) {
+        if (this.y < 16 || this.y > canvas.height - 82) {
           this.dy *= -1;
         }
       }.bind(this.sprite);
